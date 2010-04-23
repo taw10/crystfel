e928b579334eb9e889c241d5bfe8e63cf027a3d2
Thomas White <taw@physics.org>
2010-04-23 18:23:52 +0200

    process_hkl: Use new input file format

--- src/process_hkl.c
+++ src/process_hkl.c
@@ -298,7 +298,7 @@ int main(int argc, char *argv[])
 		int r;
 
 		rval = fgets(line, 1023, fh);
-		if ( strncmp(line, "New pattern", 11) == 0 ) {
+		if ( strncmp(line, "Reflections from indexing", 25) == 0 ) {
 
 			if ( n_patterns == 0 ) {
 				n_patterns++;
