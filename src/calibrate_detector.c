b214f6a7ffc3485d46a5375b0868f699b2897e63
Thomas White <taw@physics.org>
2010-07-06 18:35:15 +0200

    calibrate_detector: Add missing item from --help

--- src/calibrate_detector.c
+++ src/calibrate_detector.c
@@ -91,7 +91,8 @@ static void show_help(const char *s)
 "                           pixels in each 3x3 region to zero if any of them\n"
 "                           have negative values.\n"
 "\n"
-"  -j <n>                  Run <n> analyses in parallel.  Default 1.\n");
+"  -j <n>                  Run <n> analyses in parallel.  Default 1.\n"
+" -x, --prefix=<p>         Prefix filenames from input file with 'p'.\n");
 }
 
 
