717f89b116fef535072be5ada5b3c32a4fc6de1b
Thomas White <taw@physics.org>
2010-09-09 14:09:42 +0200

    Add missing render_hkl.h

---
+++ src/render_hkl.h
@@ -0,0 +1,26 @@
+/*
+ * render_hkl.h
+ *
+ * Draw pretty renderings of reflection lists
+ *
+ * (c) 2006-2010 Thomas White <taw@physics.org>
+ *
+ * Part of CrystFEL - crystallography with a FEL
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifndef RENDER_HKL_H
+#define RENDER_HKL_H
+
+enum {
+	WGHT_I,
+	WGHT_SQRTI,
+	WGHT_COUNTS,
+	WGHT_RAWCOUNTS,
+};
+
+#endif	/* RENDER_HKL_H */
