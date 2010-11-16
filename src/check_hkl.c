a2bb3a864af159a0bcd9db808e89a3743981b108
Thomas White <taw@physics.org>
2010-11-16 12:08:43 +0100

    Move 'characterisation' stuff to a new program, check_hkl

--- src/check_hkl.c
+++ src/check_hkl.c
@@ -1,7 +1,7 @@
 /*
- * compare_hkl.c
+ * check_hkl.c
  *
- * Compare reflection lists
+ * Characterise reflection lists
  *
  * (c) 2006-2010 Thomas White <taw@physics.org>
  *
@@ -34,24 +34,22 @@
 
 static void show_help(const char *s)
 {
-	printf("Syntax: %s [options] <file1.hkl> <file2.hkl>\n\n", s);
+	printf("Syntax: %s [options] <file.hkl>\n\n", s);
 	printf(
-"Compare intensity lists.\n"
+"Characterise an intensity list.\n"
 "\n"
 "  -h, --help                 Display this help message.\n"
-"  -o, --output=<filename>    Specify output filename for correction factor.\n"
 "  -y, --symmetry=<sym>       The symmetry of both the input files.\n"
 "  -p, --pdb=<filename>       PDB file to use (default: molecule.pdb).\n"
-"      --shells               Plot the figures of merit by resolution.\n"
 "      --rmin=<res>           Fix lower resolution limit for --shells (m^-1).\n"
 "      --rmax=<res>           Fix upper resolution limit for --shells (m^-1).\n"
 "\n");
 }
 
 
-static void plot_shells(const double *ref1, const double *ref2,
-                        ReflItemList *items, double scale, UnitCell *cell,
-                        const char *sym, double rmin_fix, double rmax_fix)
+static void plot_shells(const double *ref1, ReflItemList *items, UnitCell *cell,
+                        const char *sym, unsigned int *counts,
+                        const double *sigma, double rmin_fix, double rmax_fix)
 {
 	double num[NBINS];
 	int cts[NBINS];
@@ -69,6 +67,9 @@ static void plot_shells(const double *ref1, const double *ref2,
 	int i;
 	int ctot;
 	FILE *fh;
+	double snr_total = 0;
+	int nmeas = 0;
+	int nmeastot = 0;
 	int nout = 0;
 
 	if ( cell == NULL ) {
@@ -176,16 +177,13 @@ static void plot_shells(const double *ref1, const double *ref2,
 	}
 	free(counted);
 
-	den = 0.0;
-	ctot = 0;
-	nout = 0;
+	/* Characterise the first data set (only) */
 	for ( i=0; i<num_items(items); i++ ) {
 
 		struct refl_item *it;
 		signed int h, k, l;
 		double d;
 		int bin;
-		double i1, i2;
 		int j;
 
 		it = get_item(items, i);
@@ -200,23 +198,24 @@ static void plot_shells(const double *ref1, const double *ref2,
 				break;
 			}
 		}
-
-		/* Outside resolution range? */
 		if ( bin == -1 ) {
 			nout++;
 			continue;
 		}
 
-		i1 = lookup_intensity(ref1, h, k, l);
-		i2 = lookup_intensity(ref2, h, k, l);
-		i2 *= scale;
-
-		num[bin] += fabs(i1 - i2);
-		den += i1;
-		ctot++;
-		cts[bin]++;
+		measured[bin]++;
+		measurements[bin] += lookup_count(counts, h, k, l);
+		snr[bin] += (lookup_intensity(ref1, h, k, l) /
+		                              lookup_intensity(sigma, h, k, l));
+		snr_total += (lookup_intensity(ref1, h, k, l) /
+		                              lookup_intensity(sigma, h, k, l));
+		nmeas++;
+		nmeastot += lookup_count(counts, h, k, l);
 
 	}
+	STATUS("overall <snr> = %f\n", snr_total/(double)nmeas);
+	STATUS("%i measurements in total.\n", nmeastot);
+	STATUS("%i reflections in total.\n", nmeas);
 
 	if ( nout ) {
 		STATUS("Warning; %i reflections outside resolution range.\n",
@@ -228,11 +227,10 @@ static void plot_shells(const double *ref1, const double *ref2,
 		double r, cen;
 		cen = rmins[i] + (rmaxs[i] - rmins[i])/2.0;
 		r = (num[i]/den)*((double)ctot/cts[i]);
-		fprintf(fh, "%f %f %i %i %i %f %f\n", cen*1.0e-9, r*100.0,
-		                            measured[i],
-		                            possible[i], measurements[i],
-		                            (float)measurements[i]/measured[i],
-		                            (snr[i]/(double)measured[i]));
+		fprintf(fh, "%f %i %i %5.2f %i %f %f\n", cen*1.0e-9, measured[i],
+		        possible[i], 100.0*(float)measured[i]/possible[i],
+		        measurements[i], (float)measurements[i]/measured[i],
+		        (snr[i]/(double)measured[i]));
 
 	}
 
@@ -243,35 +241,24 @@ static void plot_shells(const double *ref1, const double *ref2,
 int main(int argc, char *argv[])
 {
 	int c;
-	double *ref1;
-	double *ref2;
-	double *ref2_transformed;
-	double *out;
+	double *ref;
 	UnitCell *cell;
-	char *outfile = NULL;
-	char *afile = NULL;
-	char *bfile = NULL;
+	char *file = NULL;
 	char *sym = NULL;
-	double scale, scale_r2, scale_rdig, R1, R2, R1i, Rdiff, pearson;
-	double scale_rintint, scale_r1i, scale_r1, scale_r1fi;
-	int i, ncom;
-	ReflItemList *i1, *i2, *icommon;
-	int config_shells = 0;
+	int i;
+	ReflItemList *items;
+	ReflItemList *good_items;
 	char *pdb = NULL;
-	double *esd1;
-	double *esd2;
-	int rej1 = 0;
-	int rej2 = 0;
-	unsigned int *cts1;
+	double *esd;
+	int rej = 0;
+	unsigned int *cts;
 	float rmin_fix = -1.0;
 	float rmax_fix = -1.0;
 
 	/* Long options */
 	const struct option longopts[] = {
 		{"help",               0, NULL,               'h'},
-		{"output",             1, NULL,               'o'},
 		{"symmetry",           1, NULL,               'y'},
-		{"shells",             0, &config_shells,     1},
 		{"pdb",                1, NULL,               'p'},
 		{"rmin",               1, NULL,               2},
 		{"rmax",               1, NULL,               3},
@@ -279,16 +266,12 @@ int main(int argc, char *argv[])
 	};
 
 	/* Short options */
-	while ((c = getopt_long(argc, argv, "ho:y:p:", longopts, NULL)) != -1) {
+	while ((c = getopt_long(argc, argv, "hy:p:", longopts, NULL)) != -1) {
 
 		switch (c) {
 		case 'h' :
 			show_help(argv[0]);
 			return 0;
-
-		case 'o' :
-			outfile = strdup(optarg);
-			break;
 
 		case 'y' :
 			sym = strdup(optarg);
@@ -321,8 +304,8 @@ int main(int argc, char *argv[])
 
 	}
 
-	if ( argc != (optind+2) ) {
-		ERROR("Please provide exactly two HKL files to compare.\n");
+	if ( argc != (optind+1) ) {
+		ERROR("Please provide exactly one HKL files to check.\n");
 		return 1;
 	}
 
@@ -330,68 +313,41 @@ int main(int argc, char *argv[])
 		sym = strdup("1");
 	}
 
-	afile = strdup(argv[optind++]);
-	bfile = strdup(argv[optind]);
+	file = strdup(argv[optind++]);
 
 	if ( pdb == NULL ) {
 		pdb = strdup("molecule.pdb");
 	}
-
 	cell = load_cell_from_pdb(pdb);
 	free(pdb);
 
-	ref1 = new_list_intensity();
-	esd1 = new_list_sigma();
-	cts1 = new_list_count();
-	i1 = read_reflections(afile, ref1, NULL, cts1, esd1);
-	if ( i1 == NULL ) {
-		ERROR("Couldn't open file '%s'\n", afile);
-		return 1;
-	}
-	ref2 = new_list_intensity();
-	esd2 = new_list_sigma();
-	i2 = read_reflections(bfile, ref2, NULL, NULL, esd2);
-	if ( i2 == NULL ) {
-		ERROR("Couldn't open file '%s'\n", bfile);
+	ref = new_list_intensity();
+	esd = new_list_sigma();
+	cts = new_list_count();
+	items = read_reflections(file, ref, NULL, cts, esd);
+	if ( items == NULL ) {
+		ERROR("Couldn't open file '%s'\n", file);
 		return 1;
 	}
 
-	/* List for output scale factor map */
-	out = new_list_intensity();
-
-	/* Find common reflections (taking symmetry into account) */
-	icommon = new_items();
-	ref2_transformed = new_list_intensity();
-	for ( i=0; i<num_items(i1); i++ ) {
+	/* Reject reflections */
+	good_items = new_items();
+	for ( i=0; i<num_items(items); i++ ) {
 
 		struct refl_item *it;
 		signed int h, k, l;
-		signed int he, ke, le;
-		double val1, val2;
-		double sig1, sig2;
+		double val, sig;
 		int ig = 0;
 		double d;
 
-		it = get_item(i1, i);
+		it = get_item(items, i);
 		h = it->h;  k = it->k;  l = it->l;
 
-		if ( !find_unique_equiv(i2, h, k, l, sym, &he, &ke, &le) ) {
-			//STATUS("%i %i %i not matched (%f nm).\n", h, k, l,
-			//       1.0/(2.0*resolution(cell, h, k, l)/1e9));
-			continue;
-		}
+		val = lookup_intensity(ref, h, k, l);
+		sig = lookup_sigma(esd, h, k, l);
 
-		val1 = lookup_intensity(ref1, h, k, l);
-		val2 = lookup_intensity(ref2, he, ke, le);
-		sig1 = lookup_sigma(esd1, h, k, l);
-		sig2 = lookup_sigma(esd2, he, ke, le);
-
-		if ( val1 < 3.0 * sig1 ) {
-			rej1++;
-			ig = 1;
-		}
-		if ( val2 < 3.0 * sig2 ) {
-			rej2++;
+		if ( val < 3.0 * sig ) {
+			rej++;
 			ig = 1;
 		}
 
@@ -401,69 +357,11 @@ int main(int argc, char *argv[])
 
 		//if ( ig ) continue;
 
-		set_intensity(ref2_transformed, h, k, l, val2);
-		set_intensity(out, h, k, l, val1/val2);
-		add_item(icommon, h, k, l);
+		add_item(good_items, h, k, l);
 
 	}
-	ncom = num_items(icommon);
-	STATUS("%i reflections with I < 3.0*sigma(I) rejected from '%s'\n",
-	       rej1, afile);
-	STATUS("%i reflections with I < 3.0*sigma(I) rejected from '%s'\n",
-	       rej2, bfile);
 
-	STATUS("%i,%i reflections: %i in common\n",
-	       num_items(i1), num_items(i2), ncom);
-
-	R1 = stat_r1_ignore(ref1, ref2_transformed, icommon, &scale_r1fi);
-	STATUS("R1(F) = %5.4f %% (scale=%5.2e) (ignoring negative intensities)\n",
-	       R1*100.0, scale_r1fi);
-
-	R1 = stat_r1_zero(ref1, ref2_transformed, icommon, &scale_r1);
-	STATUS("R1(F) = %5.4f %% (scale=%5.2e) (zeroing negative intensities)\n",
-	       R1*100.0, scale_r1);
-
-	R2 = stat_r2(ref1, ref2_transformed, icommon, &scale_r2);
-	STATUS("R2(I) = %5.4f %% (scale=%5.2e)\n", R2*100.0, scale_r2);
-
-	R1i = stat_r1_i(ref1, ref2_transformed, icommon, &scale_r1i);
-	STATUS("R1(I) = %5.4f %% (scale=%5.2e)\n", R1i*100.0, scale_r1i);
-
-	Rdiff = stat_rdiff_ignore(ref1, ref2_transformed, icommon, &scale_rdig);
-	STATUS("Rint(F) = %5.4f %% (scale=%5.2e) (ignoring negative intensities)\n",
-	       Rdiff*100.0, scale_rdig);
-
-	Rdiff = stat_rdiff_zero(ref1, ref2_transformed, icommon, &scale);
-	STATUS("Rint(F) = %5.4f %% (scale=%5.2e) (zeroing negative intensities)\n",
-	       Rdiff*100.0, scale);
-
-	Rdiff = stat_rdiff_intensity(ref1, ref2_transformed, icommon,
-	                             &scale_rintint);
-	STATUS("Rint(I) = %5.4f %% (scale=%5.2e)\n",
-	       Rdiff*100.0, scale_rintint);
-
-	pearson = stat_pearson_i(ref1, ref2_transformed, icommon);
-	STATUS("Pearson r(I) = %5.4f\n", pearson);
-
-	pearson = stat_pearson_f_ignore(ref1, ref2_transformed, icommon);
-	STATUS("Pearson r(F) = %5.4f (ignoring negative intensities)\n",
-	       pearson);
-
-	pearson = stat_pearson_f_zero(ref1, ref2_transformed, icommon);
-	STATUS("Pearson r(F) = %5.4f (zeroing negative intensities)\n",
-	       pearson);
-
-	if ( config_shells ) {
-		plot_shells(ref1, ref2_transformed, icommon, scale_r1fi,
-		            cell, sym, rmin_fix, rmax_fix);
-	}
-
-	if ( outfile != NULL ) {
-
-		write_reflections(outfile, icommon, out, NULL, NULL, cell, 1.0);
-		STATUS("Sigma(I) values in output file are not meaningful.\n");
-
-	}
+	plot_shells(ref, items, cell, sym, cts, esd, rmin_fix, rmax_fix);
 
 	return 0;
 }
