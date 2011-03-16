e474d91aa7c2689d6341b409330804a0af9f3c3b
Thomas White <taw@physics.org>
2011-03-16 11:55:34 +0100

    Update copyright dates

--- src/pattern_sim.c
+++ src/pattern_sim.c
@@ -3,7 +3,7 @@
  *
  * Simulate diffraction patterns from small crystals
  *
- * (c) 2006-2010 Thomas White <taw@physics.org>
+ * (c) 2006-2011 Thomas White <taw@physics.org>
  *
  * Part of CrystFEL - crystallography with a FEL
  *
