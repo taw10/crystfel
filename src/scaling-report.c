178c06c5102f02477fb2e87a8c4a44a4904c4e12
Thomas White <taw@physics.org>
2011-09-27 17:13:32 +0200

    Fix last commit

--- src/scaling-report.c
+++ src/scaling-report.c
@@ -226,6 +226,8 @@ static void partiality_graph(cairo_t *cr, const struct image *images, int n,
 			Ipart = get_intensity(refl);
 			Ifull = get_intensity(f);
 
+			if ( Ifull < 10 ) continue;  /* FIXME: Ugh */
+
 			pobs = Ipart/(images[i].osf*Ifull);
 			pcalc = get_partiality(refl);
 
@@ -327,8 +329,6 @@ static void partiality_histogram(cairo_t *cr, const struct image *images,
 
 			Ipart = get_intensity(refl);
 			Ifull = get_intensity(f);
-
-			if ( Ifull < 10 ) continue;  /* FIXME: Ugh */
 
 			pobs = Ipart/(images[i].osf*Ifull);
 			pcalc = get_partiality(refl);
