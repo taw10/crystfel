ce861440cef2801a67cd7925fcef88faeecdccda
Thomas White <taw@physics.org>
2011-11-22 15:45:52 +0100

    partial_sim: Revert change accidentally committed in 95cf5098a13c3f07e83de610ea242e6ce12f9eb8

--- src/partial_sim.c
+++ src/partial_sim.c
@@ -469,7 +469,7 @@ int main(int argc, char *argv[])
 	image.lambda = ph_en_to_lambda(eV_to_J(beam->photon_energy));
 	image.div = beam->divergence;
 	image.bw = beam->bandwidth;
-	image.profile_radius = 0.01e9;
+	image.profile_radius = 0.003e9;
 	image.i0_available = 0;
 	image.filename = malloc(256);
 	image.copyme = NULL;
