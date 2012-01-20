4a56d62ddc2a710b3dfecfdfa134755fd3d6c521
Thomas White <taw@bitwiz.org.uk>
2012-01-20 12:50:09 -0800

    Don't compile or use ReAx if FFTW is not available

--- libcrystfel/src/reax.h
+++ libcrystfel/src/reax.h
@@ -19,9 +19,28 @@
 
 #include "cell.h"
 
+#ifdef HAVE_FFTW
+
 extern IndexingPrivate *reax_prepare(void);
 extern void reax_cleanup(IndexingPrivate *pp);
-
 extern void reax_index(IndexingPrivate *p, struct image *image, UnitCell *cell);
+
+#else /* HAVE_FFTW */
+
+static IndexingPrivate *reax_prepare()
+{
+	return NULL;
+}
+
+static void reax_cleanup(IndexingPrivate *pp)
+{
+}
+
+static void reax_index(IndexingPrivate *p, struct image *image, UnitCell *cell)
+{
+}
+
+
+#endif /* HAVE_FFTW */
 
 #endif	/* REAX_H */
