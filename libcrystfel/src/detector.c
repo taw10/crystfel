taw10/crystfel

commit 289c8a77c252e080d4d4086b848ea8fdcb604147

Fix arithmetic subtlety in find_panel()

libcrystfel/src/detector.c
@@ -421,11 +421,14 @@ struct panel *find_panel(struct detector *det, double fs, double ss)
 {
 	int p;
 
+	/* Fractional pixel coordinates are allowed to be a little further along
+	 * than "== max_{f,s}s" for an integer. */
+
 	for ( p=0; p<det->n_panels; p++ ) {
 		if ( (fs >= det->panels[p].min_fs)
-		  && (fs <= det->panels[p].max_fs)
+		  && (fs <= det->panels[p].max_fs+1)
 		  && (ss >= det->panels[p].min_ss)
-		  && (ss <= det->panels[p].max_ss) ) {
+		  && (ss <= det->panels[p].max_ss+1) ) {
 			return &det->panels[p];
 		}
 	}

commit 720c8726ddf071e3b1cbe893c1d774ddf75ebc30

Fix find_panel() (again)
This fixes a not-so-subtle bug introduced by 289c8a77c252e080d4d4086b848ea8fdcb604147.

libcrystfel/src/detector.c
@@ -426,9 +426,9 @@ struct panel *find_panel(struct detector *det, double fs, double ss)
 
 	for ( p=0; p<det->n_panels; p++ ) {
 		if ( (fs >= det->panels[p].min_fs)
-		  && (fs <= det->panels[p].max_fs+1)
+		  && (fs < det->panels[p].max_fs+1)
 		  && (ss >= det->panels[p].min_ss)
-		  && (ss <= det->panels[p].max_ss+1) ) {
+		  && (ss < det->panels[p].max_ss+1) ) {
 			return &det->panels[p];
 		}
 	}