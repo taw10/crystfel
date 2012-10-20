taw10/crystfel

commit c7712c20c3977af8eccf9a7951b9f494e5ba14c4

indexamajig: Load the unit cell whether it's needed or not
This allows MOSFLM indexing to have access to more information.

src/indexamajig.c
@@ -549,18 +549,13 @@ int main(int argc, char *argv[])
 	}
 	free(geometry);
 
-	if ( reduction_needs_cell || indexer_needs_cell ) {
-		cell = load_cell_from_pdb(pdb);
-		if ( cell == NULL ) {
-			ERROR("Couldn't read unit cell (from %s)\n", pdb);
-			return 1;
-		}
-	} else {
-		STATUS("No cell needed for these choices of indexing"
-		       " and reduction.\n");
-		cell = NULL;
+	cell = load_cell_from_pdb(pdb);
+	if ( cell == NULL ) {
+		ERROR("Couldn't read unit cell (from %s)\n", pdb);
+		return 1;
 	}
 	free(pdb);
+	cell_print(cell);
 
 	write_stream_header(ofh, argc, argv);
 

commit d33230479371986bd58fc3ddc129ef91db4868d7

indexamajig: Don't load unit cell if none given
This fixes some incorrect behaviour introduced by c7712c20.

src/indexamajig.c
@@ -449,10 +449,6 @@ int main(int argc, char *argv[])
 	}
 	free(speaks);
 
-	if ( pdb == NULL ) {
-		pdb = strdup("molecule.pdb");
-	}
-
 	if ( prefix == NULL ) {
 		prefix = strdup("");
 	} else {
@@ -550,13 +546,18 @@ int main(int argc, char *argv[])
 	}
 	free(geometry);
 
-	cell = load_cell_from_pdb(pdb);
-	if ( cell == NULL ) {
-		ERROR("Couldn't read unit cell (from %s)\n", pdb);
-		return 1;
+	if ( pdb != NULL ) {
+		cell = load_cell_from_pdb(pdb);
+		if ( cell == NULL ) {
+			ERROR("Couldn't read unit cell (from %s)\n", pdb);
+			return 1;
+		}
+		free(pdb);
+		cell_print(cell);
+	} else {
+		STATUS("No unit cell given.\n");
+		cell = NULL;
 	}
-	free(pdb);
-	cell_print(cell);
 
 	write_stream_header(ofh, argc, argv);
 