2dc09493d78eaa26f2f8a07bfdeb7b3f7a030fa4
Thomas White <taw@physics.org>
2014-04-08 17:22:11 +0200

    Fix format string in displaywindow_error()

--- src/dw-hdfsee.c
+++ src/dw-hdfsee.c
@@ -58,7 +58,7 @@ static void displaywindow_error(DisplayWindow *dw, const char *message)
 	window = gtk_message_dialog_new(GTK_WINDOW(dw->window),
 					GTK_DIALOG_DESTROY_WITH_PARENT,
 					GTK_MESSAGE_WARNING,
-					GTK_BUTTONS_CLOSE, message);
+					GTK_BUTTONS_CLOSE, "%s", message);
 	gtk_window_set_title(GTK_WINDOW(window), "Error");
 
 	g_signal_connect_swapped(window, "response",
