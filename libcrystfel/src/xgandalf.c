8f338772bb7a94f613c6f59ac379c47f50099e43
Thomas White <taw@physics.org>
2018-06-20 09:58:06 +0200

    Add GPL headings

--- libcrystfel/src/xgandalf.c
+++ libcrystfel/src/xgandalf.c
@@ -1,8 +1,29 @@
 /*
  * xgandalf.c
  *
- *  Created on: 08.08.2017
- *      Author: gevorkov
+ * Interface to XGANDALF indexer
+ *
+ * Copyright © 2017-2018 Deutsches Elektronen-Synchrotron DESY,
+ *                       a research centre of the Helmholtz Association.
+ *
+ * Authors:
+ *   2017-2018 Yaroslav Gevorkov <yaroslav.gevorkov@desy.de>
+ *
+ * This file is part of CrystFEL.
+ *
+ * CrystFEL is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CrystFEL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CrystFEL.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #include "xgandalf.h"
