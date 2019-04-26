fa5d0f9561642e7dd5edc94297b313cf6e55b35b
Thomas White <taw@physics.org>
2019-04-26 17:20:56 +0200

    Add GPL boilerplate

--- libcrystfel/src/pinkindexer.h
+++ libcrystfel/src/pinkindexer.h
@@ -1,8 +1,29 @@
 /*
- * pinkIndexer.h
+ * pinkindexer.h
  *
- *  Created on: Nov 27, 2017
- *      Author: gevorkov
+ * Interface to PinkIndexer
+ *
+ * Copyright © 2017-2019 Deutsches Elektronen-Synchrotron DESY,
+ *                       a research centre of the Helmholtz Association.
+ *
+ * Authors:
+ *   2017-2019 Yaroslav Gevorkov <yaroslav.gevorkov@desy.de>
+ *
+ * This file is part of CrystFEL.
+ *
+ * CrystFEL is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CrystFEL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CrystFEL.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #ifndef LIBCRYSTFEL_SRC_PINKINDEXER_H_
