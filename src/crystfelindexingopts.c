82c5f584e50c49cc432619eb1924d4841d0a78eb
Thomas White <taw@physics.org>
2020-08-20 11:35:06 +0200

    Remove unused function

--- src/crystfelindexingopts.c
+++ src/crystfelindexingopts.c
@@ -635,28 +635,6 @@ static const char *integration_method_id(IntegrationMethod meth)
 }
 
 
-static void unset_all_methods(CrystFELIndexingOpts *opts)
-{
-	GtkTreePath *path;
-	GtkTreeIter iter;
-
-	path = gtk_tree_path_new_from_string("0");
-	gtk_tree_model_get_iter(GTK_TREE_MODEL(opts->indm_store),
-	                        &iter, path);
-	gtk_tree_path_free(path);
-
-	do {
-		gtk_list_store_set(opts->indm_store, &iter,
-		                   0, FALSE,
-		                   2, FALSE,
-		                   3, FALSE,
-		                   -1);
-
-	} while ( gtk_tree_model_iter_next(GTK_TREE_MODEL(opts->indm_store),
-	                                   &iter) );
-}
-
-
 void crystfel_indexing_opts_set_indexing_method_string(CrystFELIndexingOpts *opts,
                                                        const char *indm_str)
 {
