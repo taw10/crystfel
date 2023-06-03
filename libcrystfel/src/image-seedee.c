c4271d8171f2f1f0a58ae47ba77dc96136450687
Thomas White <taw@physics.org>
2023-06-03 20:27:07 +0200

    Support Seedee format f8<

--- libcrystfel/src/image-seedee.c
+++ libcrystfel/src/image-seedee.c
@@ -98,6 +98,20 @@ static int load_seedee_data(struct panel_template *p,
 			}
 		}
 
+	} else if ( (array->datatype == 'f')
+	  && (array->itemsize == 8)
+	  && (array->byteorder == '<') )
+	{
+		int fs, ss;
+		double *in_data = (double *)array->data;
+
+		for ( ss=0; ss<PANEL_HEIGHT(p); ss++ ) {
+			for ( fs=0; fs<PANEL_WIDTH(p); fs++ ) {
+				size_t idx = fs+p->orig_min_fs + (ss+p->orig_min_ss)*data_size_fs;
+				data[fs+ss*PANEL_WIDTH(p)] = in_data[idx];
+			}
+		}
+
 	} else {
 		ERROR("Unrecognised data type %c%i%c\n",
 		      array->datatype, array->itemsize, array->byteorder);
