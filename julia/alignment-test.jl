64410538e50883c3366bbbfcf67a0f72a5ea5f13
Thomas White <taw@physics.org>
2023-10-31 15:16:41 +0100

    Julia: Improve UnitCell implementation
    This adds Julian enums for lattice type, centering and unique axis, and
    a "show" method.

--- julia/alignment-test.jl
+++ julia/alignment-test.jl
@@ -3,6 +3,7 @@ using CrystFEL
 dtempl = loaddatatemplate("julia/alignment-test.geom")
 image = Image(dtempl)
 
-cell = UnitCell(123, 45, 80, 90, 97, 90)
+cell = UnitCell(123, 45, 80, 90, 97, 90,
+                centering='P', latticetype=MonoclinicLattice, uniqueaxis='b')
 
 truth = predictreflections(image, cell)
