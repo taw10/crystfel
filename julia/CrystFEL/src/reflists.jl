0f9410e0fd3da07100e56fd3c5ab398a55af1e61
Thomas White <taw@physics.org>
2023-11-03 11:37:40 +0100

    Julia: show/print/display methods for RefList and SymOpList

--- julia/CrystFEL/src/reflists.jl
+++ julia/CrystFEL/src/reflists.jl
@@ -1,16 +1,20 @@
 module RefLists
 
 import ..CrystFEL: libcrystfel
-import ..CrystFEL.Symmetry: SymOpList, InternalSymOpList
-
+import ..CrystFEL.Symmetry: SymOpList, InternalSymOpList, symmetry_name
 export RefList, loadreflist
 
+
+# The internal libcrystfel structure, not exposed directly
 mutable struct InternalRefList end
 
+
+# The Julian exposed type
 mutable struct RefList
     internalptr::Ptr{InternalRefList}
     symmetry::SymOpList
 end
+
 
 function loadreflist(filename::AbstractString)
 
@@ -26,5 +30,11 @@ function loadreflist(filename::AbstractString)
     return RefList(out, SymOpList(symmetryname))
 
 end
+
+
+function Base.show(io::IO, ::MIME"text/plain", reflist::RefList)
+    write(io, "Reflection list in point group ", symmetry_name(reflist.symmetry))
+end
+
 
 end  # of module
