e25567d76317f5c52f55476004b9c50fc94a6ec5
Thomas White <taw@physics.org>
2023-11-05 15:57:56 +0100

    Julia: Add RefList iteration

--- julia/CrystFEL/src/reflists.jl
+++ julia/CrystFEL/src/reflists.jl
@@ -5,14 +5,56 @@ import ..CrystFEL.Symmetry: SymOpList, InternalSymOpList, symmetry_name
 export RefList, loadreflist
 
 
-# The internal libcrystfel structure, not exposed directly
+# The internal libcrystfel structures, not exposed directly
+# We only ever have e.g. a Ptr{InternalRefList}, never a real InternalRefList
 mutable struct InternalRefList end
+mutable struct InternalReflection end
+mutable struct InternalRefListIterator end
 
-
-# The Julian exposed type
+# The Julian exposed types
 mutable struct RefList
     internalptr::Ptr{InternalRefList}
     symmetry::SymOpList
+end
+
+mutable struct Reflection
+    internalptr::Ptr{InternalReflection}
+end
+
+mutable struct RefListIterator
+    lastrefl::Ptr{InternalReflection}
+    internalptr::Ptr{InternalRefListIterator}
+end
+
+
+function Base.iterate(a::RefList)
+
+    ir = Ref{Ptr{InternalRefListIterator}}()
+    refl = ccall((:first_refl, libcrystfel),
+                 Ptr{InternalReflection}, (Ptr{InternalRefList},Ref{Ptr{InternalRefListIterator}}),
+                 a.internalptr, ir)
+
+    if refl == C_NULL
+        throw(ArgumentError("Failed to find first reflection in list"))
+    end
+
+    return Reflection(refl),RefListIterator(refl, ir[])
+
+end
+
+
+function Base.iterate(a::RefList, iter)
+
+    refl = ccall((:next_refl, libcrystfel),
+                 Ptr{InternalReflection}, (Ptr{InternalReflection},Ptr{InternalRefListIterator}),
+                 iter.lastrefl, iter.internalptr)
+
+    if refl == C_NULL
+        return nothing
+    end
+
+    return Reflection(refl),RefListIterator(refl, iter.internalptr)
+
 end
 
 
