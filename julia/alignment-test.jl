a117a52f03dbba5ac030f5256752b0f39613d4ba
Thomas White <taw@physics.org>
2023-11-08 16:24:15 +0100

    Update alignment-test.jl

--- julia/alignment-test.jl
+++ julia/alignment-test.jl
@@ -2,7 +2,6 @@ using CrystFEL
 
 dtempl = loaddatatemplate("julia/alignment-test.geom")
 image = Image(dtempl)
-
 cell = UnitCell(MonoclinicLattice, PrimitiveCell, 123, 45, 80, 90, 97, 90)
-
-truth = predictreflections(image, cell)
+cr = Crystal(cell, image)
+truth = predictreflections(cr, image)
