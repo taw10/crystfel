73120849630bfa0995b33701e6b5df34127d432c
Thomas White <taw@physics.org>
2023-11-09 11:07:25 +0100

    Julia: add initial Crystal type and reflection prediction

--- julia/alignment-test.jl
+++ julia/alignment-test.jl
@@ -3,5 +3,6 @@ using CrystFEL
 dtempl = loaddatatemplate("julia/alignment-test.geom")
 image = Image(dtempl)
 cell = UnitCell(MonoclinicLattice, PrimitiveCell, 123, 45, 80, 90, 97, 90)
-cr = Crystal(cell, image)
+cr = Crystal(cell)
 truth = predictreflections(cr, image)
+println(truth)
