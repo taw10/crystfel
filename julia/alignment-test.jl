6bf5342ce8e1af84f89acbb4a8b7fa70068e68e6
Thomas White <taw@physics.org>
2023-11-15 17:02:30 +0100

    Update alignment-test.jl

--- julia/alignment-test.jl
+++ julia/alignment-test.jl
@@ -1,8 +1,25 @@
 using CrystFEL
+using Random
 
+# Create empty image for simulation purposes
 dtempl = loaddatatemplate("julia/alignment-test.geom")
 image = Image(dtempl)
+
+# Create a crystal and calculate its reflections
 cell = UnitCell(MonoclinicLattice, PrimitiveCell, 123, 45, 80, 90, 97, 90)
-cr = Crystal(cell)
+cr = Crystal(cell)  # FIXME: Random rotation
 truth = predictreflections(cr, image)
-println(truth)
+
+# Sketch a diffraction pattern
+image.peaklist = PeakList()
+for refl in truth
+    if randn() > 3
+        let dpos = refl.detectorposition
+            push!(image.peaklist, dpos.fs, dpos.ss, dpos.panelnumber, 100.0)
+        end
+    end
+end
+
+# Index the pattern
+indexer = Indexing("asdf-cell", cell, retry=false, multilattice=false, refine=true)
+index(image, indexer)
