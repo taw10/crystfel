3e467fb7f9ed331212ee2434eee8afd0355b96ff
Thomas White <taw@physics.org>
2023-11-16 14:11:48 +0100

    Update alignment-test.jl

--- julia/alignment-test.jl
+++ julia/alignment-test.jl
@@ -10,16 +10,35 @@ cell = UnitCell(MonoclinicLattice, PrimitiveCell, 123, 45, 80, 90, 97, 90)
 cr = Crystal(cell)  # FIXME: Random rotation
 truth = predictreflections(cr, image)
 
-# Sketch a diffraction pattern
-image.peaklist = PeakList()
-for refl in truth
-    if randn() > 3
-        let dpos = refl.detectorposition
-            push!(image.peaklist, dpos.fs, dpos.ss, dpos.panelnumber, 100.0)
+# "Simulate" a diffraction pattern from the reflections
+function sketch_pattern(reflist)
+    peaks = PeakList()
+    for refl in reflist
+        if randn() > 0
+            let dpos = refl.detectorposition
+                push!(peaks, dpos.fs, dpos.ss, dpos.panelnumber, 100.0)
+            end
         end
     end
+    return peaks
 end
 
+image.peaklist = sketch_pattern(truth)
+
 # Index the pattern
-indexer = Indexer("asdf-cell", dtempl, cell, retry=false, multilattice=false, refine=true)
+indexer = Indexer("asdf", dtempl, cell, retry=false, multilattice=false, refine=true)
 index(image, indexer)
+
+
+# Utility routine for visualising peaks
+function plotpanel(image, pn)
+    x = []
+    y = []
+    for pk in image.peaklist
+        if pk.panelnumber == pn
+            push!(x, pk.fs)
+            push!(y, pk.ss)
+        end
+    end
+    scatter(x, y)
+end
