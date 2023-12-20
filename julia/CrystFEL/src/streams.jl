dfa3ecf8c416470609f97b3b2eeafc5914fc4ba8
Thomas White <taw@physics.org>
2023-12-20 12:57:50 +0100

    Julia: Stream semantics

--- julia/CrystFEL/src/streams.jl
+++ julia/CrystFEL/src/streams.jl
@@ -13,34 +13,66 @@ mutable struct Stream
 end
 
 
+"""
+    Stream(filename, "w", dtempl)
+
+Opens a CrystFEL stream for writing.  Note that you must provide a `DataTemplate`,
+which is needed to translate "panel coordinates" to "file coordinates".
+
+Corresponds to CrystFEL C API routine `stream_open_for_write`.
+"""
 function Stream(filename, mode::AbstractString, dtempl::DataTemplate)
+
     if mode == "w"
         out = @ccall libcrystfel.stream_open_for_write(filename::Cstring,
-                                                       dtempl.internalptr::Ptr{InternalDataTemplate})::Ptr{InternalStream}
+                               dtempl.internalptr::Ptr{InternalDataTemplate})::Ptr{InternalStream}
         if out == C_NULL
             throw(ErrorException("Failed to open stream for reading"))
         end
-        return Stream(out)
+        finalizer(close, Stream(out))
+
     elseif mode =="r"
         throw(ArgumentError("To open a stream for reading, don't provide the DataTemplate"))
+
     else
         throw(ArgumentError("Unrecognised CrystFEL stream mode"*mode))
     end
 end
 
 
+"""
+    Stream(filename, "r")
+
+Opens a CrystFEL stream for reading.
+
+Close the stream with `close` when you've finished (this will happen
+automatically when the `Stream` object is finalized).
+
+Corresponds to CrystFEL C API routine `stream_open_for_read`.
+"""
 function Stream(filename, mode::AbstractString)
+
     if mode == "r"
         out = @ccall libcrystfel.stream_open_for_read(filename::Cstring)::Ptr{InternalStream}
         if out == C_NULL
             throw(ErrorException("Failed to open stream for reading"))
         end
-        return Stream(out)
+        finalizer(close, Stream(out))
+
     elseif mode == "w"
         throw(ArgumentError("To open a stream for writing, you must provide "
                             *"a DataTemplate: use Stream(filename, \"w\", dtempl)"))
+
     else
         throw(ArgumentError("Unrecognised CrystFEL stream mode"*mode))
+    end
+end
+
+
+function Base.close(st::Stream)
+    if st.internalptr != C_NULL
+        @ccall libcrystfel.stream_close(st.internalptr::Ptr{InternalStream})::Cvoid
+        st.internalptr = C_NULL
     end
 end
 
