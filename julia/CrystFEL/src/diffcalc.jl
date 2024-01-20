8fe6287bfc5335c47140fe7ba745de14b1376d2c
Thomas White <taw@physics.org>
2024-01-20 15:37:03 +0100

    Julia: predictreflections: add types
    This is to stop me from putting the image and crystal the wrong way
    round.

--- julia/CrystFEL/src/diffcalc.jl
+++ julia/CrystFEL/src/diffcalc.jl
@@ -1,14 +1,14 @@
 module DiffractionCalculations
 
 import ..CrystFEL: libcrystfel
-import ..CrystFEL.Images: InternalImage
-import ..CrystFEL.Crystals: InternalCrystal
+import ..CrystFEL.Images: InternalImage, Image
+import ..CrystFEL.Crystals: InternalCrystal, Crystal
 import ..CrystFEL.RefLists: RefList, UnmergedReflection, InternalRefList
 import ..CrystFEL.Symmetry: SymOpList
 export predictreflections
 
 
-function predictreflections(cr, image; maxres=1e10)
+function predictreflections(cr::Crystal, image::Image; maxres=1e10)
     refls = ccall((:predict_to_res, libcrystfel),
                   Ptr{InternalRefList},
                   (Ptr{InternalCrystal}, Ptr{InternalImage}, Cdouble),
