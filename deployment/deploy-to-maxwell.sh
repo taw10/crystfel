0d3463b91eed1eabce4c9790e82bb5b7140d51f7
Thomas White <taw@physics.org>
2025-03-05 13:50:53 +0100

    CI: Move known_hosts stuff to final deployment
    The cluster where the images are built cannot access the server with the
    known hosts file.

--- deployment/deploy-to-maxwell.sh
+++ deployment/deploy-to-maxwell.sh
@@ -9,6 +9,12 @@ PREFIX="$3"
 BASE_REMOTE_DIR="/software/crystfel"
 CURRENT_TARGET_DIR="$BASE_REMOTE_DIR/devel-$(date +%F-%H:%M:%S)"
 
+if [ ! -d ~/.ssh ]; then \
+    mkdir -p ~/.ssh; \
+    chmod 700 ~/.ssh; \
+fi;  \
+wget https://wims.desy.de/system/ALL_afs/etc/ssh_known_hosts2 -O ~/.ssh/known_hosts
+
 # To avoid people starting CrystFEL devel processes while those are
 # being redeployed (leading to weird error messages), we copy first,
 # and then atomically switch a symlink (see "mv -T" below) so we have
