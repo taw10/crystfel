c4df03578127f7fd6e5dad6501b612724e8217ea
Thomas White <taw@physics.org>
2025-03-17 15:47:04 +0100

    Add tests/header_read.c

---
+++ tests/header_read.c
@@ -0,0 +1,96 @@
+/*
+ * header_read.c
+ *
+ * Check that HDF5 headers can be read correctly
+ *
+ * Copyright © 2025 Deutsches Elektronen-Synchrotron DESY,
+ *                  a research centre of the Helmholtz Association.
+ *
+ * Authors:
+ *   2025 Thomas White <taw@physics.org>
+ *
+ * This file is part of CrystFEL.
+ *
+ * CrystFEL is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CrystFEL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CrystFEL.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include <stream.h>
+#include <image.h>
+#include <datatemplate.h>
+
+int main(int argc, char *argv[])
+{
+	DataTemplate *dtempl;
+	struct image *image;
+	Stream *st;
+	long long int v;
+
+	dtempl = data_template_new_from_file(argv[1]);
+	if ( dtempl == NULL ) {
+		ERROR("Failed to load data template\n");
+		return 1;
+	}
+
+	data_template_add_copy_header(dtempl, "/bigint");
+
+	image = image_read(dtempl, argv[2], "//", 0, 0, NULL);
+	if ( image == NULL ) {
+		ERROR("Failed to load image\n");
+		return 1;
+	}
+
+	st = stream_open_for_write("header_read.stream", dtempl);
+	if ( st == NULL ) {
+		ERROR("Failed to open stream for writing\n");
+		return 1;
+	}
+
+	stream_write_geometry_file(st, argv[1]);
+
+	if ( stream_write_chunk(st, image, 0) ) {
+		ERROR("Failed to write stream chunk\n");
+		return 1;
+	}
+
+	stream_close(st);
+	image_free(image);
+
+	st = stream_open_for_read("header_read.stream");
+	if ( st == NULL ) {
+		ERROR("Failed to open stream for reading\n");
+		return 1;
+	}
+
+	image = stream_read_chunk(st, 0);
+	if ( image == NULL ) {
+		ERROR("Failed to read stream chunk\n");
+		return 1;
+	}
+	stream_close(st);
+
+	image_read_header_int(image, "/bigint", &v);
+	if ( v != 1234567890123456789 ) {
+		ERROR("Wrong value read (%lli)\n", v);
+		return 1;
+	}
+
+	image_free(image);
+	unlink("header_read.stream");
+	return 0;
+}
---
+++ tests/header_read.geom
@@ -0,0 +1,16 @@
+photon_energy = 10000 eV
+data = /data
+clen = 50 mm
+res = 1000000
+adu_per_photon = 1
+
+panel/min_fs = 0
+panel/max_fs = 1023
+panel/min_ss = 0
+panel/max_ss = 1023
+panel/fs = x
+panel/ss = y
+panel/dim0 = ss
+panel/dim1 = fs
+panel/corner_x = -500
+panel/corner_y = -500
